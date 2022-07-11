e5ea4bb547da3c55b1a71359b6d45a73afcd7cf2
Solomon04 <antoinesolomon5@gmail.com>
2022-07-11 08:52:28 -0500

    initial commit.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.jsx",
+        "./resources/**/*.tsx",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
